6fb5d19c8ac887c52944c67abdde855ac88e7536
JPry <JPry@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-11 16:08:31 +0000

    Create initial file
    git-svn-id: http://plugins.svn.wordpress.org/redirect-network-login/trunk@873481 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ redirect-network-login.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: Redirect Network Login
+ * Plugin URI: https://github.com/JCPry/redirect-network-login
+ * Description: Redirect the <pre>wp-login.php</pre> form of sub-sites to the main site login form
+ * Version: 1.0
+ * Author: Jeremy Pry
+ * Author URI: https://github.com/JCPry/
+ * License: GPL2
+ */
+
+// Prevent direct access to this file
+if ( ! defined( 'ABSPATH' ) ) {
+    die( 'You can\'t do anything by accessing this file directly.' );
+}
